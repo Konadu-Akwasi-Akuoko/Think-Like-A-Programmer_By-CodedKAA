cc2529b86064c95cfa103f64de9765e842bc02e1
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-07-12 14:25:53 +0000

    Chapter 02 - Problem 02 : Sideways triangle.

---
+++ Problem2-Ch2:Sideways_Triangle/SidewaysTriagle.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+
+/* PROBLEM: A SIDEWAYS TRIANGLE
+Write a program that uses only two output statements, cout << "#" and cout << "\n", to produce
+a pattern of hash symbols shaped like a sideways triangle:
+#
+##
+###
+####
+###
+##
+#
+*/
+
+int fromLine5 = 3;
+void printHashVal();
+
+// main() is where program execution begins.
+int main()
+{
+    //This for loop creates 7 lines of free space which can be filled with letters with the help of (cout << "\n")
+    for (int line = 1; line <= 7; line++)
+    {
+        //We first check and see if the line we are currently on is less than 4, if it is less than four we print
+        //"#" according to the line number, so if we start from line 1, we will print only 1 "#" on that line,
+        //on line 2 we will print 2 "#",etc. 
+        //NB: the "cout << "\n";" is outside the for loop inside this if statement, so the loop will execute and finish
+        //before going to the next line.
+        if (line <= 4)
+        {
+            for (int hashVal = 1; hashVal <= line; hashVal++)
+            {
+                printHashVal();
+            }
+        }
+        //In this else statement, when the value of the line is greater than 4 and the current line we are on is 5,
+        //we use "fromLine5" (which serves as the number of times we need to print "#") to print out "#", 
+        //and outside the loop but in the same else statement we decrease the number by 1, thus forming a decresing print of "#".
+        else
+        {
+            for (int hashVal2 = 1; hashVal2 <= fromLine5; hashVal2++)
+            {
+                printHashVal();
+            }
+            fromLine5 -= 1;
+        }
+        cout << "\n";
+    }
+}
+
+//This function is needed, because from the problem it states that "use only two output statements".
+void printHashVal()
+{
+    cout << "#";
+}
