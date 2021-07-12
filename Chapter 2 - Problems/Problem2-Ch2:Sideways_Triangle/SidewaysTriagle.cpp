d6319960537bc5d53eb6778c242a913e9cdfb210
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2021-07-12 15:01:04 +0000

    Chapter02 Problem02: Sideways Triangle.

--- Chapter 2 - Problems/Problem2-Ch2:Sideways_Triangle/SidewaysTriagle.cpp
+++ Chapter 2 - Problems/Problem2-Ch2:Sideways_Triangle/SidewaysTriagle.cpp
@@ -13,6 +13,9 @@ a pattern of hash symbols shaped like a sideways triangle:
 #
 */
 
+void AntonSolution();
+string response;
+
 int fromLine5 = 3;
 void printHashVal();
 
@@ -24,7 +27,7 @@ int main()
     {
         //We first check and see if the line we are currently on is less than 4, if it is less than four we print
         //"#" according to the line number, so if we start from line 1, we will print only 1 "#" on that line,
-        //on line 2 we will print 2 "#",etc. 
+        //on line 2 we will print 2 "#",etc.
         //NB: the "cout << "\n";" is outside the for loop inside this if statement, so the loop will execute and finish
         //before going to the next line.
         if (line <= 4)
@@ -35,7 +38,7 @@ int main()
             }
         }
         //In this else statement, when the value of the line is greater than 4 and the current line we are on is 5,
-        //we use "fromLine5" (which serves as the number of times we need to print "#") to print out "#", 
+        //we use "fromLine5" (which serves as the number of times we need to print "#") to print out "#",
         //and outside the loop but in the same else statement we decrease the number by 1, thus forming a decresing print of "#".
         else
         {
@@ -47,10 +50,31 @@ int main()
         }
         cout << "\n";
     }
+    //Here is the solution of Mr. Anton.
+    cout << "Would you like to see the solution of Mr.Anton? \n";
+    cout << "Y for yes, and N for no \n";
+    cin >> response;
+    if (response == "Y" | response == "y")
+    {
+        AntonSolution();
+    }
 }
 
 //This function is needed, because from the problem it states that "use only two output statements".
 void printHashVal()
 {
     cout << "#";
+}
+
+//This is the solution of Mr. Anton.
+void AntonSolution()
+{
+    for (int row = 1; row <= 7; row++)
+    {
+        for (int hashNum = 1; hashNum <= 4 - abs(4 - row); hashNum++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
 }
